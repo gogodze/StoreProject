bbdb831c9d2a41fdec02900f3698b86d1654f77e
David <david@gogodze.dev>
2025-05-07 23:49:21 +0400

    added migrations

---
+++ src/Infrastructure/Migrations/20250507182503_remove name and surname move to fullname.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removenameandsurnamemovetofullname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "users",
+                newName: "FullName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "users",
+                newName: "UserName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "users",
+                type: "TEXT",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "users",
+                type: "TEXT",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
