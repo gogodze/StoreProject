81fb917d5553b29b848586047d2f51aace7ab844
David <david@gogodze.dev>
2025-06-21 18:47:02 +0400

    added http user accessor, made everything into an encrypted queryable, made transactions possible, added global logging behavior and validator

--- src/Application/Users/Commands/RegisterCustomerCommand.cs
+++ src/Application/Users/Commands/RegisterCustomerCommand.cs
@@ -2,8 +2,10 @@
 using Destructurama.Attributed;
 using Domain.Aggregates;
 using Domain.ValueObjects;
+using EntityFrameworkCore.DataProtection.Extensions;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using static BCrypt.Net.BCrypt;
 
 namespace Application.Users.Commands;
@@ -11,36 +13,54 @@ namespace Application.Users.Commands;
 public sealed record RegisterCustomerCommand : IRequest<User>
 {
     [LogMasked]
-    public string FullName { get; set; }
+    public string FullName { get; set; } = null!;
 
     [LogMasked]
-    public string Password { get; set; }
+    public string Password { get; set; } = null!;
 
     [LogMasked]
-    public string Email { get; set; }
+    public string Email { get; set; } = null!;
 
     [LogMasked]
-    public string ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = null!;
 }
 
 public class RegisterCustomerCommandValidator : AbstractValidator<RegisterCustomerCommand>
 {
-    public RegisterCustomerCommandValidator()
+    public RegisterCustomerCommandValidator(IAppDbContext dbContext)
     {
         RuleFor(x => x.FullName)
             .NotEmpty()
             .MinimumLength(5)
-            .MaximumLength(15);
+            .MaximumLength(15)
+            .Matches(@"^[a-zA-Z\s]+$").WithMessage("Full name must contain only letters and spaces.");
+
 
         RuleFor(x => x.Password)
             .NotEmpty()
             .MinimumLength(6)
             .MaximumLength(50);
 
+
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty()
+            .Equal(x => x.Password).WithMessage("Passwords must match.")
+            .MinimumLength(6)
+            .MaximumLength(50);
+
         RuleFor(x => x.Email)
             .NotEmpty()
             .EmailAddress()
-            .MaximumLength(50);
+            .MaximumLength(50)
+            .WithMessage("Email must be a valid email address and not exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .MustAsync(async (_, email, ct) =>
+            {
+                var usersWithPd = await dbContext.Set<User>().WherePdEquals(nameof(User.Email), email).CountAsync(ct);
+                return usersWithPd == 0;
+            })
+            .WithMessage("Email already exists.");
     }
 }
 
@@ -48,17 +68,27 @@ public sealed record RegisterCustomerCommandHandler(IAppDbContext DbContext) : I
 {
     public async Task<User> Handle(RegisterCustomerCommand request, CancellationToken ct)
     {
-        var user = new User
+        var transaction = await DbContext.BeginTransactionAsync(ct);
+        try
         {
-            Id = Ulid.NewUlid(),
-            FullName = request.FullName,
-            HashedPassword = EnhancedHashPassword(request.Password),
-            Role = Role.Customer,
-            RegisterDate = DateTime.Now,
-            Email = request.Email,
-        };
-        await DbContext.Set<User>().AddAsync(user, ct);
-        await DbContext.SaveChangesAsync(ct);
-        return user;
+            var user = new User
+            {
+                Id = Ulid.NewUlid(),
+                FullName = request.FullName,
+                HashedPassword = EnhancedHashPassword(request.Password),
+                Role = Role.Customer,
+                RegisterDate = DateTime.Now,
+                Email = request.Email,
+            };
+            await DbContext.Set<User>().AddAsync(user, ct);
+            await DbContext.SaveChangesAsync(ct);
+            await transaction.CommitAsync(ct);
+            return user;
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync(ct);
+            throw;
+        }
     }
 }
---
+++ src/Infrastructure/Services/HttpContextUserAccessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Claims;
+using Application.Services;
+using Domain.Aggregates;
+using Domain.Common;
+using Domain.ValueObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Services;
+
+public sealed class HttpContextCurrentUserAccessor(IHttpContextAccessor httpContextAccessor, IAppDbContext dbContext) : ICurrentUserAccessor
+{
+    private ClaimsPrincipal? User => httpContextAccessor.HttpContext?.User;
+
+    public Ulid? Id => User?.GetId();
+
+    public Role? Role => User?.GetRole();
+
+    public async Task<User?> TryGetCurrentUserAsync(CancellationToken ct = default)
+    {
+        if (Id is not { } id)
+            return null;
+
+        var user = await dbContext.Set<User>()
+            .Where(u => u.Id == id)
+            .FirstOrDefaultAsync(ct);
+
+        return user ?? throw new InvalidOperationException($"Failed to load the user from the database, user with id: {id} not found");
+    }
+}
